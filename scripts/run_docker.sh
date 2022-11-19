1992918d59b470e1b3c088521cdf031617e177a3
Markus jayjah <markus@jayjah.de>
2022-11-19 12:52:56 +0100

    mix

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run --platform linux/arm64 -it --mount type=bind,source="$(pwd)",target=/root/home jayjah/ansible-prod
+docker run --platform linux/arm64 -it --network="host" --mount type=bind,source="$(pwd)",target=/root/home jayjah/ansible-prod
